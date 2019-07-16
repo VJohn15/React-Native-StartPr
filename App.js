f1052df549f6818047affa72d673ecf5edf746ab
VJohn15 <megaivmay15@gmail.com>
2019-07-16 17:24:43 +0300

    mazafaka

--- App.js
+++ App.js
@@ -1,44 +1,122 @@
 import React, { Component } from 'react';
-import {StyleSheet, TextInput, Text, View, Button } from 'react-native';
+import {StyleSheet, TextInput, Text, View, Button, TouchableOpacity } from 'react-native';
 
-function func2(){
-  console.log(this)
-}
+
+
 
 export default class App extends Component {
 
   constructor(){
     super()
-    this.state = {}
-    this.buttonPressed = this.buttonPressed.bind(this)
+    this.state = {
+      resultText: "",
+      calculationText: ""
+    }
+    this.operations = ['<-', '+', '-', '*', '/']
   }
 
-  buttonPressed(){
-    //Get username and password values
-      //const username = this._username._lastNativeText
-      //const password = this._password._lastNativeText
-      
-      console.log(this.state.username, this.state.password)
+  calculateResult(){
+    const text = this.state.resultText
+    console.log(text, eval(text))
+    this.setState({
+      calculationText: eval(text)
+    })
   }
+
+  validate(){
+    const text = this.state.resultText
+    switch(text.slice(-1)){
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+        return false
+    }
+    return true
+  }
+
+  buttonPressed(text) {
+    //console.log(text)
+
+    if (text == '='){
+      return this.validate() && this.calculateResult()
+    }
+
+    this.setState({
+      resultText: this.state.resultText + text
+    })
+  }
+
+  operate (operations){
+    switch(operations) {
+      case '<-':
+        console.log(this.state.resultText)
+        let text = this.state.resultText.split('')
+        text.pop()
+        this.setState({
+          resultText: text.join('')
+        })
+
+        break
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+
+        const lastChar = this.state.resultText.split('').pop()
+
+        if (this.operations.indexOf(lastChar) > 0) return
+
+        if (this.state.text == "") return
+        this.setState({
+          resultText: this.state.resultText + operations
+        })
+    }
+  }
+
+ 
 
   render() {
+
+    let rows = []
+    let nums = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['.', 0, '=']]
+      for(let i = 0; i < 4; i++){
+        let row = []
+        for(let j = 0; j < 3; j++){
+          row.push(
+          <TouchableOpacity key = {nums[i] [j]} onPress = {() => this.buttonPressed(nums[i] [j])} style = {styles.btn}>
+            <Text style = {styles.btntext}>{nums[i] [j]}</Text>
+          </TouchableOpacity> 
+          )
+        }
+        rows.push(<View key = {i} style = {styles.row}>{row}</View>)
+      }
+
+     
+      let ops = []
+      for (let i = 0; i < 5; i++){
+        ops.push(<TouchableOpacity key = {this.operations[i]} style = {styles.btn} onPress = {() => this.operate(this.operations[i])}>
+          <Text style = {[styles.btntext, styles.white]}>{this.operations[i]}</Text>
+        </TouchableOpacity>)
+      }
+
     return (
       <View style={styles.container}>
-
-        <Text>Username</Text>
-        <TextInput 
-         defaultValue = {this.state.username}
-         onChangeText = {text => this.setState({username: text})}
-        />
-
-        <Text>Password</Text>
-        <TextInput 
-          defaultValue = {this.state.password}
-          onChangeText = {text => this.setState({password: text})}
-        />
-
-        <Button title = {"Submit"} onPress = {this.buttonPressed}/>
-
+          <View style = {styles.result}>
+            <Text style = {styles.resultText}>{this.state.resultText}</Text>
+          </View>
+          <View style = {styles.calculation}>
+          <Text style = {styles.calculationText}>{this.state.calculationText}</Text>
+          </View>
+          <View style = {styles.buttons}>
+              <View style = {styles.numbers}>
+                {rows}
+              </View>
+              <View style = {styles.operations}>
+                 {ops}
+              </View>
+          </View>
+        
       </View>
     );
   }
@@ -46,8 +124,59 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  btntext: {
+    fontSize :35,
+    color: 'white'
+  },  
+  white: {
+    color: 'white'
+  },
+  btn: {
+    flex: 1,
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  resultText: {
+    fontSize :40,
+    color: 'black'
+  },
+  calculationText:{
+    fontSize: 34,
+    color: 'black'
+  },  
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  result: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    backgroundColor: 'white'
+  },
+  calculation: {
     flex: 1,
     justifyContent: 'center',
-    padding: 20
+    alignItems: 'flex-end',
+    backgroundColor: 'white'
+  },
+  buttons: {
+    flex: 7,
+    flexDirection: 'row'
+  },
+  numbers: {
+    flex: 3,
+    backgroundColor: '#434343'
+  },
+  operations: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'stretch',
+    backgroundColor: '#636363'
   }
 })
